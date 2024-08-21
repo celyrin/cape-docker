19acbc454c4dc5a7a89f35e9efbe11b783f807a1
celyrin <celyrin6@gmail.com>
2024-08-21 22:10:05 +0800

    chore: Update ownership and symbolic links in entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -21,41 +21,60 @@ if [ ! -d $work ]; then
     echo "Work directory not found"
     exit 1
 else 
-    sudo chown cape $work
+    sudo chown -R cape $work
 fi
 
 cwd="/opt/CAPEv2"
 if [ ! -d $cwd ]; then
     echo "CAPEv2 not found"
     exit 1
-else 
-    if [ ! -d $cwd/conf ]; then
-        echo "CAPEv2 configuration not found"
-        exit 1
-    else
-        mv $cwd/conf $work
-        ln -s $work/conf $cwd/conf
-    fi
+fi 
 
-    if [ ! -d $cwd/storage ]; then
-        echo "CAPEv2 storage not found"
-        sudo -u cape mkdir -p $cwd/storage
-        mv $cwd/storage $work
-        ln -s $work/storage $cwd/storage
-    else
-        mv $cwd/storage $work
-        ln -s $work/storage $cwd/storage
-    fi
+if [ -L $cwd/conf ]; then
+    echo "CAPEv2 configuration is a symbolic link"
+elif [ -d $work/conf ]; then
+    echo "CAPEv2 configuration backup found"
+    rm -rf $cwd/conf
+    chown -R cape $work/conf
+    ln -s $work/conf $cwd/conf
+elif [ -d $cwd/conf ]; then
+    echo "CAPEv2 configuration found"
+    mv $cwd/conf $work
+    ln -s $work/conf $cwd/conf
+fi
 
-    if [ ! -d $cwd/log ]; then
-        echo "CAPEv2 log not found"
-        sudo -u cape mkdir -p $cwd/log
-        mv $cwd/log $work
-        ln -s $work/log $cwd/log
-    else
-        mv $cwd/log $work
-        ln -s $work/log $cwd/log
-    fi
+if [ -L $cwd/storage ]; then
+    echo "CAPEv2 storage is a symbolic link"
+elif [ -d $work/storage ]; then
+    echo "CAPEv2 storage backup found"
+    rm -rf $cwd/storage
+    chown -R cape $work/storage
+    ln -s $work/storage $cwd/storage
+elif [ -d $cwd/storage ]; then
+    echo "CAPEv2 storage found"
+    mv $cwd/storage $work
+    ln -s $work/storage $cwd/storage
+else
+    echo "CAPEv2 storage not found"
+    sudo -u cape mkdir -p $work/storage
+    ln -s $work/storage $cwd/storage
+fi
+
+if [ -L $cwd/log ]; then
+    echo "CAPEv2 log is a symbolic link"
+elif [ -d $work/log ]; then
+    echo "CAPEv2 log backup found"
+    rm -rf $cwd/log
+    chown -R cape $work/log
+    ln -s $work/log $cwd/log
+elif [ -d $cwd/log ]; then
+    echo "CAPEv2 log found"
+    mv $cwd/log $work
+    ln -s $work/log $cwd/log
+else
+    echo "CAPEv2 log not found"
+    sudo -u cape mkdir -p $work/log
+    ln -s $work/log $cwd/log
 fi
 
 if ! systemctl is-active --quiet cape-rooter.service; then
@@ -77,6 +96,5 @@ if ! systemctl is-active --quiet cape-processor.service; then
     echo "Starting cape-processor.service"
     systemctl restart cape-processor.service
 fi
-
 
 echo "End of entrypoint"
